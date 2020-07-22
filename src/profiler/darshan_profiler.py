be42b5c0d1c0050c8097248ffc579945414af40d
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-07-22 17:04:34 -0500

    added singleton code to profiler.

--- src/profiler/darshan_profiler.py
+++ src/profiler/darshan_profiler.py
@@ -2,8 +2,22 @@ from src.profiler.io_profiler import IOProfiler
 
 
 class DarshanProfiler(IOProfiler):
-    def __init__(self, logdir):
+    __instance = None
+
+    @staticmethod
+    def get_instance(logdir):
+        """ Static access method. """
+        if DarshanProfiler.__instance == None:
+            DarshanProfiler(logdir)
+        return DarshanProfiler.__instance
+
+    def __init__(self,logdir):
         super().__init__(logdir)
+        """ Virtually private constructor. """
+        if DarshanProfiler.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            DarshanProfiler.__instance = self
 
     def start(self):
         pass
--- src/profiler/tf_profiler.py
+++ src/profiler/tf_profiler.py
@@ -2,8 +2,22 @@ from src.profiler.io_profiler import IOProfiler
 
 
 class TFProfiler(IOProfiler):
-    def __init__(self, logdir):
+    __instance = None
+
+    @staticmethod
+    def get_instance(logdir):
+        """ Static access method. """
+        if TFProfiler.__instance == None:
+            TFProfiler(logdir)
+        return TFProfiler.__instance
+
+    def __init__(self,logdir):
         super().__init__(logdir)
+        """ Virtually private constructor. """
+        if TFProfiler.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            TFProfiler.__instance = self
 
     def start(self):
         pass
