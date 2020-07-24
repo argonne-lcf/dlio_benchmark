2930a4f28ba57437452b35cc979af926f2c4106f
hdevarajan92 <hdevarajan@hawk.iit.edu>
2020-07-24 14:07:52 -0500

    fixed code to include debug variables.

--- src/data_generator/hdf5_generator.py
+++ src/data_generator/hdf5_generator.py
@@ -1,5 +1,6 @@
 import h5py
 from numpy import random
+import math
 
 from src.data_generator.data_generator import DataGenerator
 from src.utils.utility import progress
@@ -8,6 +9,7 @@ from src.utils.utility import progress
 class HDF5Generator(DataGenerator):
     def __init__(self):
         super().__init__()
+        self.chunk_size = self._arg_parser.args.chunk_size
 
     def generate(self):
         super().generate()
@@ -17,6 +19,7 @@ class HDF5Generator(DataGenerator):
             progress(i+1, self.num_files, "Generating HDF5 Data")
             out_path_spec = "{}_{}_of_{}.h5".format(self._file_prefix, i, self.num_files)
             hf = h5py.File(out_path_spec, 'w')
-            hf.create_dataset('records', data=records, chunks=(1, self._dimension, self._dimension))
+            chunk_dimension = int(math.sqrt(self.chunk_size))
+            hf.create_dataset('records', data=records, chunks=(1, chunk_dimension, chunk_dimension))
             hf.create_dataset('labels', data=record_labels)
             hf.close()
