9f7a2bd6f8c94db134a17e4d40cb07c4600fe9c9
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-07-28 14:06:27 -0500

    fixed sub setting read.

--- src/dlio_benchmark.py
+++ src/dlio_benchmark.py
@@ -78,7 +78,6 @@ class DLIOBenchmark(object):
             step += 1
             if step > total:
                 return step - 1
-            MPI.COMM_WORLD.barrier()
         return step - 1
 
     def run(self):
