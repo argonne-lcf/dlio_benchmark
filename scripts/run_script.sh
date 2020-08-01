aaea43244e2f408aaec9fa28afd0cbd9e61e5ed0
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-07-31 20:22:08 -0500

    add run script

--- scripts/run_script.sh
+++ scripts/run_script.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/sh
 vals_0=(0 1KB 2KB 4KB 8KB 16KB 32KB 64KB 128KB 256KB 512KB 1MB 2MB 4MB 8MB 16MB)
+echo "Waiting"
+while [ "$(qstat -u dhari | wc -l)" -ne 2 ]
+do
+  sleep 2
+done
 for u in "${vals_0[@]}"
 do
 qsub ffn.sh $u
