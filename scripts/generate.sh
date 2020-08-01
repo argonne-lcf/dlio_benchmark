b66047da33b72b752c7cb817ca9dd29b4150a627
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-07-31 19:06:11 -0500

    add generate script

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/sh
 vals_1=(16 32 64 128 256 512)
 vals_2=(1 2 4 8 16)
 
-source /soft/datascience/tensorflow/tf2.2-craympi.sh
+source /soft/datascience/tensorflow/tf2.2-login.sh
 export MPICH_MAX_THREAD_SAFETY=multiple
 CURRENT_DIR=`pwd`
 DLIO_ROOT=`dirname $CURRENT_DIR`
