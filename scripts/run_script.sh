d20f2f4b6d463e2365af61c7b4cd10301cc450ec
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-07-31 20:18:41 -0500

    add run script

--- scripts/run_script.sh
+++ scripts/run_script.sh
@@ -5,7 +5,7 @@ CURRENT_DIR=`pwd`
 DLIO_ROOT=`dirname $CURRENT_DIR`
 export PYTHONPATH=$DLIO_ROOT:$PYTHONPATH
 vals_0=(0 1KB 2KB 4KB 8KB 16KB 32KB 64KB 128KB 256KB 512KB 1MB 2MB 4MB 8MB 16MB)
-for u in "${vals_1[@]}"
+for u in "${vals_0[@]}"
 do
 ./ffn.sh $u
 while [ "$(qstat -u dhari | wc -l)" -ne 2 ]
