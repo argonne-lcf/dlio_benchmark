cdda2512739a5c5cc1cde4b14619910c3006d8a7
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-07-31 22:00:36 -0500

    added compression methods

--- scripts/cosmic.sh
+++ scripts/cosmic.sh
@@ -2,6 +2,8 @@
 #COBALT -n 8 -A datascience -t 1:00:00 -q debug-cache-quad
 ##COBALT -n 128 -A datascience -t 3:00:00 -q default --jobname=dlio --attrs mcdram=cache:numa=quad
 
+FILE_INDEX=$1
+
 source /soft/datascience/tensorflow/tf2.2-craympi.sh
 #source /soft/datascience/tensorflow/tf2.2-login.sh
 export MPICH_MAX_THREAD_SAFETY=multiple
@@ -21,14 +23,18 @@ DARSHAN_PRELOAD=/soft/perftools/darshan/darshan-3.1.8/lib/libdarshan.so
 
 DATA_DIR=/projects/datascience/dhari/dlio_datasets
 
-echo "aprun -n $NRANKS -N $RANKS_PER_NODE -j $THREADS_PER_CORE -cc depth -e OMP_NUM_THREADS=$PROCESS_DISTANCE -d $PROCESS_DISTANCE"
 
 #Cosmic Tagger
-APP_DATA_DIR=${DATA_DIR}/cosmic_16MB
+APP_DATA_DIR=${DATA_DIR}/cosmic_${FILE_INDEX}
 
 OPTS=(-f hdf5 -fa shared -nf 1 -sf 6000 -rl 40960 -bs 1 -df ${APP_DATA_DIR} -gd 0 -k 1)
+
+echo "aprun -n $NRANKS -N $RANKS_PER_NODE -j $THREADS_PER_CORE -cc depth -e OMP_NUM_THREADS=$PROCESS_DISTANCE -d $PROCESS_DISTANCE python ${DLIO_ROOT}/src/dlio_benchmark.py ${OPTS[@]}"
 
 #mpirun -n $NRANKS \
 aprun -n $NRANKS -N $RANKS_PER_NODE -j $THREADS_PER_CORE -cc depth -e OMP_NUM_THREADS=$PROCESS_DISTANCE -d $PROCESS_DISTANCE \
 -e DXT_ENABLE_IO_TRACE=1 -e LD_PRELOAD=$DARSHAN_PRELOAD \
 python ${DLIO_ROOT}/src/dlio_benchmark.py ${OPTS[@]}
+
+cp /lus/theta-fs0/logs/darshan/theta/2020/8/1/*${COBALT_JOBID}* /home/dhari/darshan-logs/benchmark/cosmic/optimization/${FILE_INDEX}.darshan
+
