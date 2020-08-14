875583bd3ec7ebea4175bde6de3d63d7e01e54af
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-08-13 23:31:42 -0500

    added documentations for the benchmark code.

--- src/dlio_benchmark.py
+++ src/dlio_benchmark.py
@@ -1,3 +1,16 @@
+"""
+ Copyright (C) 2020  Argonne, Hariharan Devarajan <hdevarajan@anl.gov>
+ This file is part of DLProfile
+ DLIO is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+ published by the Free Software Foundation, either version 3 of the published by the Free Software Foundation, either
+ version 3 of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU General Public License along with this program.
+ If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from src.common.enumerations import Profiler
 from src.data_generator.generator_factory import GeneratorFactory
 from src.reader.reader_factory import ReaderFactory
@@ -12,12 +25,27 @@ hvd.init()
 
 
 def barrier():
+    """
+    Barrier implementation using horovod's all-reduce
+    """
     const = tf.constant(1)
     reduced = hvd.allreduce(const)
 
 
 class DLIOBenchmark(object):
+    """
+    The Benchmark represents the I/O behavior of deep learning applications.
+    """
     def __init__(self):
+        """
+        This initializes the DLIO benchmark. Intialization includes:
+        <ul>
+            <li> argument parser </li>
+            <li> profiler instances </li>
+            <li> internal components </li>
+            <li> local variables </li>
+        </ul>
+        """
         self.arg_parser = ArgumentParser.get_instance()
         self.darshan = None
         self.tensorboard = None
@@ -35,6 +63,11 @@ class DLIOBenchmark(object):
         self.reader_handler = ReaderFactory.get_format(self.arg_parser.args.format)
 
     def initialize(self):
+        """
+        Initializes the benchmark runtime.
+        - It generates the required data
+        - Start profiling session for Darshan and Tensorboard.
+        """
         if self.arg_parser.args.debug and self.arg_parser.args.my_rank == 0:
             input("Press enter to start\n")
         barrier()
@@ -48,6 +81,9 @@ class DLIOBenchmark(object):
         barrier()
 
     def _checkpoint(self, step_number):
+        """
+        Performs Checkpointing for a specific step number. It writes different file of different sizes.
+        """
         if not os.path.exists(self.arg_parser.args.output_folder):
             os.makedirs(self.arg_parser.args.output_folder)
         model_file = os.path.join(self.arg_parser.args.output_folder,
@@ -77,8 +113,11 @@ class DLIOBenchmark(object):
         pass
 
     def _train(self):
+        """
+        Training loop for reading the dataset and performing training computations.
+        :return: returns total steps.
+        """
         step = 1
-
         total = math.ceil(self.num_samples * self.num_files / self.batch_size / self.comm_size)
         for element in self.reader_handler.next():
             if self.arg_parser.args.checkpoint and step % self.arg_parser.args.steps_checkpoint == 0:
@@ -89,6 +128,10 @@ class DLIOBenchmark(object):
         return step - 1
 
     def run(self):
+        """
+        Run the total epochs for training. On each epoch, it prepares dataset for reading, it trains, and finalizes the
+        dataset.
+        """
         if not self.arg_parser.args.generate_only:
             for epoch_number in range(0, self.arg_parser.args.epochs):
                 self.reader_handler.read(epoch_number)
@@ -99,6 +142,9 @@ class DLIOBenchmark(object):
                 self.reader_handler.finalize()
 
     def finalize(self):
+        """
+        It finalizes the dataset once the training epoch is completed.
+        """
         print("Finalizing for rank {}".format(self.arg_parser.args.my_rank))
         barrier()
         if not self.arg_parser.args.generate_only:
@@ -116,6 +162,9 @@ class DLIOBenchmark(object):
 
 
 if __name__ == '__main__':
+    """
+    The main method to start the benchmark runtime.
+    """
     os.environ["DARSHAN_DISABLE"] = "1"
     benchmark = DLIOBenchmark()
     benchmark.initialize()
