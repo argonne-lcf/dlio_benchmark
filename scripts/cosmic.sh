22de660e49e97f7d76f1575f58b288465d488ca9
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-10-15 18:51:29 -0500

    added

--- scripts/cosmic.sh
+++ scripts/cosmic.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
-#COBALT -n 2048 -A datascience -t 3:00:00 -q default --jobname=dlio_cosmic --attrs mcdram=cache:numa=quad
+#COBALT -n 128 -A datascience -t 3:00:00 -q default --jobname=dlio_cosmic --attrs mcdram=cache:numa=quad
 ##COBALT -n 8 -A datascience -t 1:00:00 -q debug-cache-quad
 
 source /soft/datascience/tensorflow/tf2.2-craympi.sh
@@ -17,19 +17,32 @@ THREADS_PER_CORE=2
 NUM_THREADS=$((NUM_CORES*THREADS_PER_CORE))
 PROCESS_DISTANCE=$((NUM_THREADS/RANKS_PER_NODE))
 
-DARSHAN_PRELOAD=/soft/perftools/darshan/darshan-3.1.8/lib/libdarshan.so
+#DARSHAN_PRELOAD=/soft/perftools/darshan/darshan-3.1.8/lib/libdarshan.so
 
-DATA_DIR=/projects/datascience/dhari/dlio_datasets
+DATA_DIR=/projects/datascience/dhari/stimulus_datasets
 
 
 #Cosmic Tagger
-APP_DATA_DIR=${DATA_DIR}/cosmic
 
-OPTS=(-f hdf5 -fa shared -nf 1 -sf 6000 -rl 40960 -bs 1 -df ${APP_DATA_DIR} -gd 0 -k 1)
+for n in 1 2 4 8 16 32 64; do
 
-echo "aprun -n $NRANKS -N $RANKS_PER_NODE -j $THREADS_PER_CORE -cc depth -e OMP_NUM_THREADS=$PROCESS_DISTANCE -d $PROCESS_DISTANCE python ${DLIO_ROOT}/src/dlio_benchmark.py ${OPTS[@]}"
+  APP_DATA_DIR=${DATA_DIR}/cosmic_${n}
+  OPTS=(-f hdf5 -fa shared -nf 1 -sf $((1500*n)) -rl 40960 -bs 1 -df ${APP_DATA_DIR} -gd 0 -k 1 -p 1 -l /projects/datascience/dhari/tf_logs/cosmic_${n})
+  echo "aprun -n $((n*RANKS_PER_NODE)) -N $RANKS_PER_NODE -j $THREADS_PER_CORE -cc depth -e OMP_NUM_THREADS=$PROCESS_DISTANCE -d $PROCESS_DISTANCE python ${DLIO_ROOT}/src/dlio_benchmark.py ${OPTS[@]}"
+  #mpirun -n $NRANKS \
+  aprun -n $((n*RANKS_PER_NODE))  -N $RANKS_PER_NODE -j $THREADS_PER_CORE -cc depth -e OMP_NUM_THREADS=$PROCESS_DISTANCE -d $PROCESS_DISTANCE \
+  python ${DLIO_ROOT}/src/dlio_benchmark.py ${OPTS[@]} &
+done;
+wait
 
-#mpirun -n $NRANKS \
-aprun -n $NRANKS -N $RANKS_PER_NODE -j $THREADS_PER_CORE -cc depth -e OMP_NUM_THREADS=$PROCESS_DISTANCE -d $PROCESS_DISTANCE \
--e DXT_ENABLE_IO_TRACE=1 -e LD_PRELOAD=$DARSHAN_PRELOAD \
-python ${DLIO_ROOT}/src/dlio_benchmark.py ${OPTS[@]}
+
+#APP_DATA_DIR=${DATA_DIR}/cosmic
+#
+#
+#
+#echo "aprun -n $NRANKS -N $RANKS_PER_NODE -j $THREADS_PER_CORE -cc depth -e OMP_NUM_THREADS=$PROCESS_DISTANCE -d $PROCESS_DISTANCE python ${DLIO_ROOT}/src/dlio_benchmark.py ${OPTS[@]}"
+#
+##mpirun -n $NRANKS \
+#aprun -n $NRANKS -N $RANKS_PER_NODE -j $THREADS_PER_CORE -cc depth -e OMP_NUM_THREADS=$PROCESS_DISTANCE -d $PROCESS_DISTANCE \
+#-e DXT_ENABLE_IO_TRACE=1 -e LD_PRELOAD=$DARSHAN_PRELOAD \
+#python ${DLIO_ROOT}/src/dlio_benchmark.py ${OPTS[@]}
