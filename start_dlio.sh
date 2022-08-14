5af9d00a8a1fce6b2b2247f16453e40e6adc3057
jovonho <loic.hovon@gmail.com>
2022-08-14 10:19:10 -0400

    Renamed the docker image to dlio:base

--- start_dlio.sh
+++ start_dlio.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir results
-docker run -it --gpus all -v results:/workspace/dlio/results dlio:test /bin/bash run_benchmark.sh
+docker run -it --gpus all -v results:/workspace/dlio/results dlio:base /bin/bash run_dlio.sh
