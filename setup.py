c02703fb95d82e7fa4a1edce7a6edbf0a0002d8c
Hariharan Devarajan <mani.hariharan@gmail.com>
2023-05-26 11:40:48 -0700

    added basic build script.

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
-from setuptools.config import read_configuration
-from setuptools import setup
+from setuptools import setup, find_packages
 
-
-conf_dict = read_configuration("./setup.cfg")
-setup(**conf_dict['metadata'])
-
-import os
-os.system("pip install -r ./requirements.txt")
+setup(
+    name='dlio_benchmark',
+    version='0.0.1',
+    include_package_data=True,
+    install_requires=[
+        'mpi4py',
+        'numpy',
+        'h5py',
+        'pandas',
+        'tensorflow >= 2.3.1',
+        'pytorch >= 1.3.1'
+    ],
+    packages=find_packages(
+        # All keyword arguments below are optional:
+        where='src',  # '.' by default
+        exclude=['tests'],  # empty by default
+    ),
+    entry_points={
+        'console_scripts': [
+            'cli-name = src.dlio_benchmark:main',
+        ]
+    }
+)
