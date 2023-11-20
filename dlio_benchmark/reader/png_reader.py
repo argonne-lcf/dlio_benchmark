6601de8d9837d7e27c225b0538a499db71f4dc9e
Huihuo Zheng <zhenghh04@gmail.com>
2023-11-20 23:49:30 +0000

    reverted some unnecessary changes

--- dlio_benchmark/reader/png_reader.py
+++ dlio_benchmark/reader/png_reader.py
@@ -50,7 +50,6 @@ class PNGReader(FormatReader):
         super().get_sample(filename, sample_index)
         image = self.open_file_map[filename]
         dlp.update(image_size=image.nbytes)
-        return image
 
     def next(self):
         for batch in super().next():
