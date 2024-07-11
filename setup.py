791b7f7f543d35212c481a3382b486c3d6417918
Izzet Yildirim <iyildirim@anl.gov>
2024-07-11 17:52:42 +0000

    PyPI publish workflow added.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/argonne-lcf/dlio_benchmark",
     author="Huihuo Zheng, Hariharan Devarajan (Hari)",
-    email="zhenghh04@gmail.com, mani.hariharan@gmail.com",
+    author_email="zhenghh04@gmail.com, mani.hariharan@gmail.com",
     classifiers=[  # Optional
         # How mature is this project? Common values are
         #   3 - Alpha
@@ -60,10 +60,10 @@ setup(
         #   5 - Production/Stable
         "Development Status :: 5 - Production/Stable",
         # Indicate who your project is intended for
-        "Intended Audience :: HPC",
+        "Intended Audience :: Science/Research",
         "Topic :: Software Development :: Build Tools",
         # Pick your license as you wish
-        "License :: OSI Approved :: Apache 2.0 License",
+        "License :: OSI Approved :: Apache Software License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate you support Python 3. These classifiers are *not*
         # checked by 'pip install'. See instead 'python_requires' below.
@@ -71,6 +71,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3 :: Only",
     ],
     keywords="deep learning, I/O, benchmark, NPZ, pytorch benchmark, tensorflow benchmark",
