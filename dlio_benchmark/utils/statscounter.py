22ac1f31b0fd73c07f588979d78e70c336e2a355
Huihuo Zheng <zhenghh04@gmail.com>
2024-08-27 22:02:00 +0000

    changed per step log to debug

--- dlio_benchmark/utils/statscounter.py
+++ dlio_benchmark/utils/statscounter.py
@@ -322,7 +322,7 @@ class StatsCounter(object):
         else:
             self.output[epoch]['proc'] = [duration]
             self.output[epoch]['compute']=[computation_time]
-        logging.info(f"{utcnow()} Rank {self.my_rank} step {step} processed {self.batch_size} samples in {duration} s")
+        logging.debug(f"{utcnow()} Rank {self.my_rank} step {step} processed {self.batch_size} samples in {duration} s")
 
     def compute_metrics_train(self, epoch, block):
         key = f"block{block}"
