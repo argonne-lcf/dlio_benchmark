7bfd89d7f0114b829e1d65396d5ae4f141a556a8
Ray Andrew <rs@rs.ht>
2024-10-15 22:57:23 +0000

    fix misleading generator message
    The message should be `Generating HDF5 data` instead of NPZ

--- dlio_benchmark/data_generator/hdf5_generator.py
+++ dlio_benchmark/data_generator/hdf5_generator.py
@@ -64,7 +64,7 @@ class HDF5Generator(DataGenerator):
             dim2 = dim[2*i+1]
             records = np.random.randint(255, size=(dim1, dim2, self.num_samples), dtype=np.uint8)
             out_path_spec = self.storage.get_uri(self._file_list[i])
-            progress(i+1, self.total_files_to_generate, "Generating NPZ Data")
+            progress(i+1, self.total_files_to_generate, "Generating HDF5 Data")
             hf = h5py.File(out_path_spec, 'w')
             hf.create_dataset('records', (self.num_samples, dim1, dim2), chunks=chunks, compression=compression,
                                     compression_opts=compression_level, dtype=np.uint8, data=records)
