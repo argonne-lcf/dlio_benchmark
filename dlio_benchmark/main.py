2ef95fd044ba16afa00be5952ca0194ecd51bb88
hariharandev1 <hariharandev1@llnl.gov>
2025-03-26 12:44:00 -0700

    fix: update checkpointing mechanism initialization to handle generate_only and checkpoint_only flags

--- dlio_benchmark/main.py
+++ dlio_benchmark/main.py
@@ -225,8 +225,11 @@ class DLIOBenchmark(object):
                 file_list_eval = file_list_eval[:self.num_files_eval]
         self.args.derive_configurations(file_list_train, file_list_eval)
         self.args.validate()
-        self.checkpointing_mechanism = CheckpointingFactory().get_mechanism(self.args.checkpoint_mechanism)
-        self.stats.checkpoint_size = self.checkpointing_mechanism.checkpoint_size    
+        self.checkpointing_mechanism = None
+        self.stats.checkpoint_size = 0
+        if (not self.generate_only) and (not self.args.checkpoint_only):
+            self.checkpointing_mechanism = CheckpointingFactory().get_mechanism(self.args.checkpoint_mechanism)
+            self.stats.checkpoint_size = self.checkpointing_mechanism.checkpoint_size    
         self.comm.barrier()
 
     @dlp.log
